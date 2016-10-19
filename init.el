b1e9d880b281a9dfa46fea196b1a71f8bbfa2a34
Tony Kemp <tony@agrav.net>
2016-10-19 14:55:19 +1100

    Use nlinum layer for line numbering.
    Much more efficient package for relative line numbering.

--- init.el
+++ init.el
@@ -55,6 +55,8 @@ values."
      version-control
      git
 
+     ;; Misc
+     nlinum
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
